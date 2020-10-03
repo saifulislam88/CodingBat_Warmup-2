3d56a01c8d21bf79b3db0b5f01b9884dc3931c31
Saiful Islam <68442870+saifulislam88@users.noreply.github.com>
2020-10-03 22:54:50 +0600

    Update string_splosion.py

--- string_splosion.py
+++ string_splosion.py
@@ -1,1 +1,8 @@
 
+str = input('Enter a String:')
+def string_splosion(str):
+    i, result = 0, ''
+    for i in range(len(str)):
+        result = result + str[:i+1]
+    return result
+print(string_splosion(str))
